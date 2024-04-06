abb446dd8e8ddfa80838437e3cf086deb74e700d
Shakeel Sanjrani <52729783+shakeel143@users.noreply.github.com>
2024-04-06 13:46:31 +0500

    Add files via upload

---
+++ AllPosibblePw.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	
+	
+	int size=10;
+	int k=0;
+	
+	int nums[size]={0,1,2,3,4,5,6,7,8,9};
+	
+	for(int a=0;a<size;a++){
+		
+		for(int b=0;b<size;b++){
+		
+		printf("%d",nums[k]);
+
+		
+	}
+	printf("\n");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	for(int i=0;i<a;i++){
+	
+		for(int j=0;j<9;j++){
+			
+			printf("%d",j+9);
+			
+			
+			
+			
+		}
+			k++;	
+			printf("\n");
+	}
+	
+	*/
+	
+}
